e5c986644d69a8b9aa67ceb7762c0f83d34493b9
Luc Bloom <luc@storygiantgames.com>
2022-04-04 17:22:34 +0200

    init

---
+++ Editor/PrefabOnlyDrawer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(PrefabOnlyAttribute))]
+public class PrefabOnlyDrawer : PropertyDrawer
+{
+	public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+	{
+		return property.isInstantiatedPrefab ? 0 : EditorGUI.GetPropertyHeight(property, label);
+	}
+	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+	{
+		if (property.isInstantiatedPrefab) return;
+		EditorGUI.PropertyField(position, property, label);
+	}
+}
+
+[CustomPropertyDrawer(typeof(InstanceOnlyAttribute))]
+public class InstanceOnlyDrawer : PropertyDrawer
+{
+	public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+	{
+		return !property.isInstantiatedPrefab ? 0 : EditorGUI.GetPropertyHeight(property, label);
+	}
+	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+	{
+		if (!property.isInstantiatedPrefab) return;
+		EditorGUI.PropertyField(position, property, label);
+	}
+}
